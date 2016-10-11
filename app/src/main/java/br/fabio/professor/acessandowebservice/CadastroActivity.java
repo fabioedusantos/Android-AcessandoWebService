77bb9fcdcce37ca2f53385312b5f8b93b98fd575
fabioedusantos <fabioedusantos@gmail.com>
2016-10-10 22:38:58 -0300

    Logando e Inserindo
    Logando e Inserindo

--- app/src/main/java/br/fabio/professor/acessandowebservice/CadastroActivity.java
+++ app/src/main/java/br/fabio/professor/acessandowebservice/CadastroActivity.java
@@ -1,6 +1,5 @@
 package br.fabio.professor.acessandowebservice;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,62 +7,65 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import br.fabio.professor.util.Http;
 import br.fabio.professor.util.HttpParam;
 
-public class MainActivity extends AppCompatActivity {
+public class CadastroActivity extends AppCompatActivity {
 
+    private EditText nome;
     private EditText usuario;
     private EditText senha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_cadastro);
 
+        nome = (EditText) findViewById(R.id.txt_nome);
         usuario = (EditText) findViewById(R.id.txt_usuario);
         senha = (EditText) findViewById(R.id.txt_senha);
     }
 
-    public void entrar(View v){
-        final String user = usuario.getText().toString();
-        final String pass = senha.getText().toString();
+    public void salvar(View v){
+        final String textNome = nome.getText().toString();
+        final String textUsuario = usuario.getText().toString();
+        final String textSenha = senha.getText().toString();
 
         new Thread(new Runnable() {
             @Override
             public void run() {
                 HttpParam p = new HttpParam();
-                p.add("usuario", user);
-                p.add("senha", pass);
+                p.add("nome", textNome);
+                p.add("usuario", textUsuario);
+                p.add("senha", textSenha);
                 String param = p.getParam();
 
-                final String ret = Http.post("http://10.10.2.47/projeto/index.php/WebService/login", param);
+                final String ret = Http.post("http://10.10.2.47/projeto/index.php/webservice/add", param);
 
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
                         try {
                             JSONObject ob = new JSONObject(ret);
-                            boolean logado = ob.getBoolean("login");
-
-                            if(logado){
-                                Toast.makeText(MainActivity.this, "Logado com Sucesso!", Toast.LENGTH_SHORT).show();
-                                Intent i = new Intent(MainActivity.this, CadastroActivity.class);
-                                startActivity(i);
+                            long status = ob.getInt("status");
+                            //O WebService retorna o id da inserção no banco se sucesso!
+                            if(status > 0){
+                                Toast.makeText(getBaseContext(), "Salvo com Sucesso!", Toast.LENGTH_SHORT).show();
                             } else{
-                                Toast.makeText(MainActivity.this, "Usuário/Senha inválido(s)!", Toast.LENGTH_SHORT).show();
+                                Toast.makeText(getBaseContext(), "Erro ao Salvar!", Toast.LENGTH_SHORT).show();
                             }
-                        } catch (Exception e){
+                        } catch (JSONException e) {
                             Log.e("apk", e.getMessage());
                         }
                     }
                 });
-
             }
         }).start();
 
 
     }
+
 }
